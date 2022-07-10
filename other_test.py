3cf773c8a19cf92ab90c66a1ba80a512ddd3c3a3
Zejun Yu <snda2008@live.cn>
2022-07-10 21:29:34 +0800

    add proberg to other_test

--- other_test.py
+++ other_test.py
@@ -1,16 +1,88 @@
-import digit_interface as Digit
-
-# digits = Digit.DigitHandler.list_digits()
-# print(digits)
-
-
-# d20299 = Digit.Digit("D20299")
-# d20299.connect()
-# d20299.set_intensity(5)
-# d20299.show_view()
+import copy
+import numpy as np
+import open3d as o3
+import transforms3d as t3d
 
 
-d00047 = Digit.Digit("D00047")
-d00047.connect()
-d00047.set_intensity(0)
-d00047.show_view()
+def estimate_normals(pcd, params):
+    pcd.estimate_normals(search_param=params)
+    pcd.orient_normals_to_align_with_direction()
+
+
+def prepare_source_and_target_rigid_3d(
+    source_filename,
+    noise_amp=0.001,
+    n_random=500,
+    orientation=np.deg2rad([0.0, 0.0, 30.0]),
+    translation=np.zeros(3),
+    voxel_size=0.005,
+    normals=False,
+):
+    source = o3.io.read_point_cloud(source_filename)
+    source = source.voxel_down_sample(voxel_size=voxel_size)
+    print(source)
+    target = copy.deepcopy(source)
+    tp = np.asarray(target.points)
+    np.random.shuffle(tp)
+    rg = 1.5 * (tp.max(axis=0) - tp.min(axis=0))
+    rands = (np.random.rand(n_random, 3) - 0.5) * rg + tp.mean(axis=0)
+    target.points = o3.utility.Vector3dVector(
+        np.r_[tp + noise_amp * np.random.randn(*tp.shape), rands]
+    )
+    ans = np.identity(4)
+    ans[:3, :3] = t3d.euler.euler2mat(*orientation)
+    ans[:3, 3] = translation
+    target.transform(ans)
+    if normals:
+        estimate_normals(
+            source, o3.geometry.KDTreeSearchParamHybrid(radius=0.3, max_nn=50)
+        )
+        estimate_normals(
+            target, o3.geometry.KDTreeSearchParamHybrid(radius=0.3, max_nn=50)
+        )
+    return source, target
+
+
+def prepare_source_and_target_nonrigid_2d(source_filename, target_filename):
+    source = np.loadtxt(source_filename)
+    target = np.loadtxt(target_filename)
+    return source, target
+
+
+def prepare_source_and_target_nonrigid_3d(
+    source_filename, target_filename, voxel_size=5.0
+):
+    source = o3.geometry.PointCloud()
+    target = o3.geometry.PointCloud()
+    source.points = o3.utility.Vector3dVector(np.loadtxt(source_filename))
+    target.points = o3.utility.Vector3dVector(np.loadtxt(target_filename))
+    source = source.voxel_down_sample(voxel_size=voxel_size)
+    target = target.voxel_down_sample(voxel_size=voxel_size)
+    print(source)
+    print(target)
+    return source, target
+
+
+import time
+import cupy as cp
+from probreg import cpd
+from probreg import callbacks
+import matplotlib.pyplot as plt
+
+# source, target = cp.asarray(
+#     prepare_source_and_target_nonrigid_2d(
+#         "output/saved_X.out",
+#         "output/saved_Y.out",
+#     ),
+#     dtype=cp.float64,
+# )
+tic = time.time()
+source, target = prepare_source_and_target_nonrigid_2d(
+    "output/saved_X.out",
+    "output/saved_Y.out",
+)
+# cbs = [callbacks.Plot2DCallback(source, target)]
+tf_param, _, _ = cpd.registration_cpd(source, target, "nonrigid", use_cuda=True)
+toc = time.time()
+print("Time:", toc - tic)
+# plt.show()
