99dd798e30e48b997b3d614a8e1e87571e1e4bb9
Zejun Yu <snda2008@live.cn>
2022-07-11 20:37:53 +0800

    add detect_edge, add black seperation

--- connect_digit.py
+++ connect_digit.py
@@ -1,6 +1,9 @@
 import sys
+import cv2
+import numpy as np
 from global_params import *
 import digit_interface as Digit
+from detect_blob import setDetectionParams, dotDetection
 
 
 def connectDigit(intensity=8):
@@ -18,7 +21,32 @@ def connectDigit(intensity=8):
 
 def main():
     digit = connectDigit()
-    digit.show_view()
+    # digit.show_view()
+
+    blobDetector = cv2.SimpleBlobDetector_create(setDetectionParams())
+
+    while True:
+        frm = digit.get_frame()
+        hsv = cv2.cvtColor(frm, cv2.COLOR_BGR2HSV)
+        lower = np.array([0, 0, 0])
+        upper = np.array([100, 100, 100])
+
+        mask = cv2.inRange(hsv, lower, upper)
+        res = cv2.bitwise_and(frm, frm, mask=mask)
+        rev = abs(255 - res)
+
+        keypoints, Frm_with_keypoints = dotDetection(blobDetector, rev)
+
+        cv2.imshow("Keypoints", Frm_with_keypoints)
+        cv2.moveWindow("Keypoints", 2020, 500)
+
+        cv2.imshow("Preview", rev)
+        cv2.moveWindow("Preview", 2020, 100)
+        getKey = cv2.waitKey(1)
+        if getKey == 27 or getKey == ord("q"):  # ESC or q
+            break
+
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
---
+++ detect_edge.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def main():
+    tri = np.loadtxt("output/saved_tri.out", delimiter=",")
+    pass
+
+
+if __name__ == "__main__":
+    main()
