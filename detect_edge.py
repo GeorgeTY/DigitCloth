007ba4a05b98c32c9af0eda6465fe336362455bd
RatioMiith <ratiomiith@gmail.com>
2022-07-29 20:43:41 +0800

    start to add RANSAC

--- detect_edge.py
+++ detect_edge.py
@@ -3,6 +3,83 @@ import numpy as np
 from global_params import *
 from sklearn.cluster import DBSCAN
 from matplotlib import pyplot as plt
+from numpy.random import default_rng as rng
+from copy import copy
+
+
+class RANSAC:
+    def __init__(self, n=10, k=100, t=0.05, d=10, model=None, loss=None, metric=None):
+        self.n = n  # `n`: Minimum number of data points to estimate parameters
+        self.k = k  # `k`: Maximum iterations allowed
+        self.t = t  # `t`: Threshold value to determine if points are fit well
+        self.d = (
+            d  # `d`: Number of close data points required to assert model fits well
+        )
+        self.model = model  # `model`: class implementing `fit` and `predict`
+        self.loss = (
+            loss  # `loss`: function of `y_true` and `y_pred` that returns a vector
+        )
+        self.metric = (
+            metric  # `metric`: function of `y_true` and `y_pred` and returns a float
+        )
+        self.best_fit = None
+        self.best_error = np.inf
+
+    def fit(self, X, y):
+
+        for _ in range(self.k):
+            ids = rng.permutation(X.shape[0])
+
+            maybe_inliers = ids[: self.n]
+            maybe_model = copy(self.model).fit(X[maybe_inliers], y[maybe_inliers])
+
+            thresholded = (
+                self.loss(y[ids][self.n :], maybe_model.predict(X[ids][self.n :]))
+                < self.t
+            )
+
+            inlier_ids = ids[self.n :][np.flatnonzero(thresholded).flatten()]
+
+            if inlier_ids.size > self.d:
+                inlier_points = np.hstack([maybe_inliers, inlier_ids])
+                better_model = copy(self.model).fit(X[inlier_points], y[inlier_points])
+
+                this_error = self.metric(
+                    y[inlier_points], better_model.predict(X[inlier_points])
+                )
+
+                if this_error < self.best_error:
+                    self.best_error = this_error
+                    self.best_fit = maybe_model
+
+        return self
+
+    def predict(self, X):
+        return self.best_fit.predict(X)
+
+
+def square_error_loss(y_true, y_pred):
+    return (y_true - y_pred) ** 2
+
+
+def mean_square_error(y_true, y_pred):
+    return np.sum(square_error_loss(y_true, y_pred)) / y_true.shape[0]
+
+
+class LinearRegressor:
+    def __init__(self):
+        self.params = None
+
+    def fit(self, X: np.ndarray, y: np.ndarray):
+        r, _ = X.shape
+        X = np.hstack([np.ones((r, 1)), X])
+        self.params = np.linalg.inv(X.T @ X) @ X.T @ y
+        return self
+
+    def predict(self, X: np.ndarray):
+        r, _ = X.shape
+        X = np.hstack([np.ones((r, 1)), X])
+        return X @ self.params
 
 
 def edgeVisualize(frm, result, method=1, scale=1):
@@ -160,6 +237,14 @@ def edgeDetection(tri, points, dotPair, area, area_diff, frm, method=1, deg=1, s
 
         return lines, frm_result
         # End Method 2
+    elif method == 3:
+        # Method 3: RANSAC
+        result, result_cov = np.polyfit(
+            triCenter[:, 0], triCenter[:, 1], 1, full=False, cov=True
+        )  #    np.polynomial.polynomial.Polynomial.fit()
+
+        return result, frm_result
+        # End Method 3
 
 
 def main():
