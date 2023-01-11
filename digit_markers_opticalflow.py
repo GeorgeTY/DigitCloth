63925bcdc1611e5da530a9a78eacf0a1d6fe3cbf
YuZejun <ratiomiith@gmail.com>
2023-01-11 16:07:28 +0800

    Visualize Pos,Vel,Acc with sliding window
    Co-authored-by: frothywater <frothywater@users.noreply.github.com>

--- digit_markers_opticalflow.py
+++ digit_markers_opticalflow.py
@@ -1,185 +1,223 @@
+import sys
 import cv2
 import time
 import numpy as np
+from collections import deque
 from connect_gsmini import connectGSmini
 from detect_blob import setDetectionParams, dotDetection
 
 
 class Markers_OF:  # Optical Flow
-    # region Variables
-    frame_init, frame_prev, frame_curr = None, None, None
-    keypoints_init, keypoints_prev, keypoints_curr = None, None, None
     sample_length = 128
-    pointer_count = 3  # The calculation of Acceleration requires (at least) 3 samples
-
     dots_count = 284
-    dots_index = 0  # Index of the current sample
-    dots_ptr = np.zeros((pointer_count + 1, dots_count), np.int8)  # Pointer to keypoint
-    dots_pos = np.zeros((sample_length, dots_count, 2), np.int8)  # Position
-    dots_vel = np.zeros((sample_length, dots_count, 2), np.float16)  # Velocity
-    dots_acc = np.zeros((sample_length, dots_count, 2), np.float16)  # Acceleration
-    # endregion
+
+    frame_queue = deque(maxlen=sample_length)
+    keypoints_queue = deque(maxlen=sample_length)
+    keypoints_mask_queue = deque(maxlen=sample_length)
+    timestamp_queue = deque(maxlen=sample_length)
+    acceleration_queue = deque(maxlen=sample_length)
+    velocity_queue = deque(maxlen=sample_length)
 
     def __init__(self):
-        self.blobDetector = cv2.SimpleBlobDetector_create(setDetectionParams())
-        self.feature_params = dict(
-            maxCorners=100, qualityLevel=0.3, minDistance=7, blockSize=7
-        )
+        self.blob_detector = cv2.SimpleBlobDetector_create(setDetectionParams())
+        self.feature_params = dict(maxCorners=100, qualityLevel=0.3, minDistance=7, blockSize=7)
         self.lk_params = dict(
             winSize=(15, 15),
             maxLevel=2,
             criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03),
         )
-        self.isInitFrame = True
-        self.isTiming = False
-        self.isVisualize = True
-        return
+        self.is_first_frame = True
+        self.is_timing = False
+        self.is_visualize = True
 
     def __track_fallback(self, frame):
         # TODO: Track using old method
         raise NotImplementedError
-        return
 
     def track(self, frame):
         """Track the markers in the current frame."""
-        if self.isTiming:
-            tic = time.time()
+        tic = time.time()
+        self.timestamp_queue.append(tic)
 
-        # region Get Keypoints
-        self.frame_curr = frame
-        # if self.isInitFrame:  # Init Frame: Get Keypoints by blob detection
-        if True:
-            self.keypoints_curr, self.frame_curr_with_keypoints = dotDetection(
-                self.blobDetector, frame
-            )
-            self.keypoints_curr = np.array(
-                [[[kp.pt[0], kp.pt[1]]] for kp in self.keypoints_curr], dtype=np.float32
-            )
+        if len(self.frame_queue) == self.sample_length:
+            self.frame_queue.popleft()
+            self.keypoints_queue.popleft()
+            self.keypoints_mask_queue.popleft()
+            self.timestamp_queue.popleft()
+        self.frame_queue.append(frame)
+
+        keypoints, self.frame_curr_with_keypoints = dotDetection(self.blob_detector, frame)
+        if self.is_first_frame:
+            # Capture the first frame and find the initial keypoints
+            keypoint_count = len(keypoints)
+            # (num_points, 1, 2)
+            keypoints = np.array([[[p.pt[0], p.pt[1]]] for p in keypoints], dtype=np.float32)
+            keypoints_mask = np.ones((keypoint_count,), dtype=np.bool)
+
+            self.keypoints_queue.append(keypoints)
+            self.keypoints_mask_queue.append(keypoints_mask)
         else:
-            self.keypoints_curr = self.keypoints_prev
-        # endregion
+            # Optical flow
+            prev_frame, next_frame = self.frame_queue[-2], self.frame_queue[-1]
+            prev_keypoints = self.keypoints_queue[-1]
+            prev_keypoints_mask = self.keypoints_mask_queue[-1]
 
-        # region Optical Flow
-        if (
-            not self.isInitFrame
-        ):  # After Init Frame: Calculate Optical Flow between previous and current frame
-            self.p1_curr, self.st_curr, self.err_curr = cv2.calcOpticalFlowPyrLK(
-                self.frame_prev,
-                self.frame_curr,
-                self.keypoints_prev,
-                self.keypoints_curr,
+            available_keypoints_indices = np.nonzero(prev_keypoints_mask)[0]
+            available_prev_keypoints = prev_keypoints[available_keypoints_indices]
+            mask_indices_mapping = np.arange(len(prev_keypoints))[available_keypoints_indices]
+            available_next_keypoints = np.zeros_like(available_prev_keypoints)
+
+            available_next_keypoints, status, _ = cv2.calcOpticalFlowPyrLK(
+                prevImg=prev_frame, nextImg=next_frame,
+                prevPts=available_prev_keypoints, nextPts=available_next_keypoints,
                 **self.lk_params
             )
-        self.keypoints_length = (
-            len(self.keypoints_curr)
-            if len(self.keypoints_curr) <= self.sample_length
-            else self.sample_length
-        )  # Limit the number of keypoints to the maximum sample length
-        # endregion
 
-        if self.isTiming:
+            next_keypoints = np.zeros_like(prev_keypoints)
+            next_keypoints_mask = np.zeros_like(prev_keypoints_mask)
+            for i in range(len(available_next_keypoints)):
+                if status[i]:
+                    index = mask_indices_mapping[i]
+                    next_keypoints[index] = available_next_keypoints[i]
+                    next_keypoints_mask[index] = 1
+            self.keypoints_queue.append(next_keypoints)
+            self.keypoints_mask_queue.append(next_keypoints_mask)
+
+        if self.is_timing:
             print("Markers: Update Time: ", time.time() - tic)
-        return
 
-    def pairing(self):
-        """Update the paring of markers from the init frame through successive frames."""
-        if self.isTiming:
-            tic = time.time()
+    def calc(self, stride: int = 1):
+        """Calculate the velocity, and acceleration of the markers."""
 
-        if not self.isInitFrame:
-            for i in range(self.keypoints_length):
-                # TODO: Figure out how the ptr works. Passing the index of the current sample to the ptr
-                self.dots_pos[self.dots_index, i, :] = self.keypoints_curr[i, :]
-                if self.st_curr[i] == 1:
-                    self.dots_ptr[i] = self.dots_ptr[i - 1]
+        if len(self.velocity_queue) == self.sample_length:
+            self.velocity_queue.popleft()
+        if len(self.acceleration_queue) == self.sample_length:
+            self.acceleration_queue.popleft()
 
-        if self.isTiming:
-            print("Markers: Track Time: ", time.time() - tic)
-        return
+        if len(self.keypoints_queue) >= stride + 1:
+            prev_keypoints, curr_keypoints = self.keypoints_queue[-1 - stride], self.keypoints_queue[-1]
+            elapsed = self.timestamp_queue[-1] - self.timestamp_queue[-1 - stride]
+            velocity = (curr_keypoints - prev_keypoints) / elapsed
+            self.velocity_queue.append(velocity)
 
-    def calc(self):
-        """Calculate the displacement, speed, and acceleration of the markers."""
-
-        return
+        if len(self.velocity_queue) >= stride + 2:
+            prev_velocity, curr_velocity = self.velocity_queue[-1 - stride], self.velocity_queue[-1]
+            elapsed = (self.timestamp_queue[-1] - self.timestamp_queue[-2 - stride]) / (stride + 1)
+            acceleration = (curr_velocity - prev_velocity) / elapsed
+            self.acceleration_queue.append(acceleration)
 
     def visualize(self):
         """Visualize the keypoints movement on the current frame."""
-        if self.isTiming:
+        if self.is_timing:
             tic = time.time()
 
         # region Visualize: Keypoints Movement
-        if not self.isInitFrame:
-            self.frame_curr_with_flow_vectors = cv2.addWeighted(
-                self.frame_prev, 0.5, self.frame_curr_with_keypoints, 0.5, 0
-            )
+        if len(self.keypoints_queue) > 1:
+            prev_frame = self.frame_queue[-2]
+            prev_keypoints, curr_keypoints = self.keypoints_queue[-2], self.keypoints_queue[-1]
+            curr_keypoints_mask = self.keypoints_mask_queue[-1]
+            frame_curr_with_flow_vectors = cv2.addWeighted(prev_frame, 0.5, self.frame_curr_with_keypoints, 0.5, 0)
 
-            for i, (new, old) in enumerate(zip(self.p1_curr, self.keypoints_prev)):
-                a, b = new.ravel()
-                c, d = old.ravel()
-                a = int(a)
-                b = int(b)
-                c = int(c)
-                d = int(d)
-                cv2.arrowedLine(
-                    self.frame_curr_with_flow_vectors,
-                    (a - (a - c) * 2, b - (b - d) * 2),
-                    (c, d),
-                    (124, 67, 237),  # KizunaAI Color
-                    2,
-                    cv2.LINE_AA,
-                    tipLength=0.25,
-                )
+            for i, (new, old) in enumerate(zip(prev_keypoints, curr_keypoints)):
+                if curr_keypoints_mask[i]:
+                    a, b = new.flatten()
+                    c, d = old.flatten()
+                    a, b, c, d = int(a), int(b), int(c), int(d)
+                    cv2.arrowedLine(
+                        frame_curr_with_flow_vectors,
+                        (a - (a - c) * 2, b - (b - d) * 2),
+                        (c, d),
+                        (124, 67, 237),  # KizunaAI Color
+                        2,
+                        cv2.LINE_AA,
+                        tipLength=0.25,
+                    )
 
+            available_keypoints_count = np.count_nonzero(curr_keypoints_mask)
             cv2.putText(
-                self.frame_curr_with_flow_vectors,
-                str(len(self.p1_curr)),
+                frame_curr_with_flow_vectors,
+                str(available_keypoints_count),
                 (0, 15),
                 cv2.FONT_HERSHEY_COMPLEX,
                 0.5,
                 (255, 255, 255),
             )
-            cv2.imshow("Optical Flow", self.frame_curr_with_flow_vectors)
+            cv2.imshow("Optical Flow", frame_curr_with_flow_vectors)
         # endregion
 
-        if self.isTiming:
+        stride = 2
+        # region Visualize: Keypoints Velocity
+        if len(self.velocity_queue) >= stride:
+            frame_curr_with_velocity_keypoints = self.frame_queue[-1].copy()
+            velocity_list = [self.velocity_queue[-i - 1] for i in range(stride)]
+            velocity_list = np.mean(np.stack(velocity_list), axis=0)
+            for i, (keypoint, velocity) in enumerate(zip(curr_keypoints, velocity_list)):
+                if curr_keypoints_mask[i]:
+                    a, b = keypoint.flatten()
+                    c, d = velocity.flatten()
+                    a, b,c,d = int(a), int(b), int(c), int(d)
+                    cv2.arrowedLine(
+                        frame_curr_with_velocity_keypoints,
+                        (a, b),
+                        (a + c, b + d),
+                        (100,255,100),
+                        2,
+                        cv2.LINE_AA,
+                        tipLength=0.25,
+                    )
+            cv2.imshow("Velocity", frame_curr_with_velocity_keypoints)
+        # endregion
+
+        # region Visualize: Keypoints Acceleration:
+        if len(self.acceleration_queue) >= stride:
+            frame_curr_with_acceleration_keypoints = self.frame_queue[-1].copy()
+            acceleration_list = [self.acceleration_queue[-i - 1] for i in range(stride)]
+            acceleration_list = np.mean(np.stack(acceleration_list), axis=0)
+            for i, (keypoint, acceleration) in enumerate(zip(curr_keypoints, acceleration_list)):
+                if curr_keypoints_mask[i]:
+                    a, b = keypoint.flatten()
+                    c, d = acceleration.flatten()
+                    a, b,c,d = int(a), int(b),int(c),int(d)
+                    cv2.arrowedLine(
+                        frame_curr_with_acceleration_keypoints,
+                        (a, b),
+                        (a + c,b+d),
+                        (100,100,255),
+                        2,
+                        cv2.LINE_AA,
+                        tipLength=0.25,
+                    )
+            cv2.imshow("Acceleration", frame_curr_with_acceleration_keypoints)
+        # endregion
+
+        # region WindowMovement
+        cv2.moveWindow("Optical Flow", 200, 100)
+        cv2.moveWindow("Velocity", 600, 100)
+        cv2.moveWindow("Acceleration", 1000, 100)
+        # endregion
+
+        if self.is_timing:
             print("Markers: Visualize Time: ", time.time() - tic)
-        return
-
-    def update(self):
-        """Update the previous frame and keypoints."""
-
-        self.frame_prev = self.frame_curr
-        self.keypoints_prev = self.keypoints_curr
-        if not self.isInitFrame:
-            self.p1_prev, self.st_prev, self.err_prev = (
-                self.p1_curr,
-                self.st_curr,
-                self.err_curr,
-            )
-        else:
-            for i in range(self.keypoints_length):  # index 0 for init frame
-                self.dots_pos[0, i, :] = self.keypoints_curr[i, :]
-                self.dots_index += 1
-
-        if self.isInitFrame:
-            self.isInitFrame = False
 
     def run(self, frame):
         """Run marker tracking by calling all the functions."""
 
         self.track(frame)
-        self.pairing()
-        self.calc()
-        if self.isVisualize:
-            self.visualize()
-        self.update()
-
-        return
+        if self.is_first_frame:
+            self.is_first_frame = False
+        else:
+            self.calc()
+            if self.is_visualize:
+                self.visualize()
+            # raise RuntimeError("Stop")
 
 
 def main():
     gsmini = connectGSmini()
     markers = Markers_OF()
+
+    for _ in range(10):
+        _ = gsmini.get_image((384, 288))
 
     try:
         while True:
@@ -192,13 +230,12 @@ def main():
                 break
             toc = time.time()
             print("FPS: ", 1 / (toc - tic))
+            # sys.write(f"FPS: {1 / (toc - tic)}\r")
     except KeyboardInterrupt:
         print("Keyboard Interrupt")
     finally:
         cv2.destroyAllWindows()
         gsmini.stop_video()
-
-    return
 
 
 if __name__ == "__main__":
