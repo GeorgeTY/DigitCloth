3eb7d43d1d1738571e3c0f4d912c1e60bba8a3f5
Zejun Yu <snda2008@live.cn>
2023-02-22 18:21:53 +0800

    function trim down: remove vel,acc,clustering

--- digit_markers_opticalflow.py
+++ digit_markers_opticalflow.py
@@ -16,9 +16,6 @@ class Markers_OF:  # Optical Flow
     frame_queue = deque(maxlen=sample_length)
     keypoints_queue = deque(maxlen=sample_length)
     keypoints_mask_queue = deque(maxlen=sample_length)
-    timestamp_queue = deque(maxlen=sample_length)
-    acceleration_queue = deque(maxlen=sample_length)
-    velocity_queue = deque(maxlen=sample_length)
 
     def __init__(self):
         self.blob_detector = cv2.SimpleBlobDetector_create(setDetectionParams())
@@ -33,20 +30,14 @@ class Markers_OF:  # Optical Flow
         self.is_visualize = True
         self.is_reset = False
 
-    def __track_fallback(self, frame):
-        # TODO: Track using old method
-        raise NotImplementedError
-
     def track(self, frame):
         """Track the markers in the current frame."""
         tic = time.time()
-        self.timestamp_queue.append(tic)
 
         if len(self.frame_queue) == self.sample_length:
             self.frame_queue.popleft()
             self.keypoints_queue.popleft()
             self.keypoints_mask_queue.popleft()
-            self.timestamp_queue.popleft()
         self.frame_queue.append(frame)
 
         keypoints, self.frame_curr_with_keypoints = dotDetection(self.blob_detector, frame)
@@ -90,53 +81,6 @@ class Markers_OF:  # Optical Flow
         if self.is_timing:
             print("Markers: Update Time: ", time.time() - tic)
 
-    def calc(self, stride: int = 1):
-        """Calculate the velocity, and acceleration of the markers."""
-
-        if len(self.velocity_queue) == self.sample_length:
-            self.velocity_queue.popleft()
-        if len(self.acceleration_queue) == self.sample_length:
-            self.acceleration_queue.popleft()
-
-        if len(self.keypoints_queue) >= stride + 1:
-            prev_keypoints, curr_keypoints = self.keypoints_queue[-1 - stride], self.keypoints_queue[-1]
-            elapsed = self.timestamp_queue[-1] - self.timestamp_queue[-1 - stride]
-            velocity = (curr_keypoints - prev_keypoints) / elapsed
-            self.velocity_queue.append(velocity)
-
-        if len(self.velocity_queue) >= stride + 2:
-            prev_velocity, curr_velocity = self.velocity_queue[-1 - stride], self.velocity_queue[-1]
-            elapsed = (self.timestamp_queue[-1] - self.timestamp_queue[-2 - stride]) / (stride + 1)
-            acceleration = (curr_velocity - prev_velocity) / elapsed
-            self.acceleration_queue.append(acceleration)
-
-    def clustering(self):
-        keypoints = self.keypoints_queue[-1].reshape(-1, 2)
-        keypoints_mask = self.keypoints_mask_queue[-1]
-        available_keypoints_indices = np.nonzero(keypoints_mask)[0]
-        available_keypoints = keypoints[available_keypoints_indices]
-
-        clustering = DBSCAN(eps=12.5, min_samples=3).fit(available_keypoints)  # TODO: Tune parameters (important)
-        unique_labels = set(clustering.labels_)
-        core_samples_mask = np.zeros_like(clustering.labels_, dtype=bool)
-        core_samples_mask[clustering.core_sample_indices_] = True
-
-        if self.is_visualize and True:
-            plt.cla()
-            colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
-            for k, col in zip(unique_labels, colors):
-                if k == -1:
-                    col = [0, 0, 0, 1]
-
-                class_member_mask = clustering.labels_ == k
-
-                xy = available_keypoints[class_member_mask & core_samples_mask]
-                plt.plot(xy[:, 0], 288 - xy[:, 1], "o", markerfacecolor=tuple(col), markeredgecolor="k", markersize=14)
-
-                xy = available_keypoints[class_member_mask & ~core_samples_mask]
-                plt.plot(xy[:, 0], 288 - xy[:, 1], "o", markerfacecolor=tuple(col), markeredgecolor="k", markersize=6)
-            plt.title("Estimated number of clusters: %d" % len(unique_labels))
-
     def visualize(self):
         """Visualize the keypoints movement on the current frame."""
         if self.is_timing:
@@ -176,55 +120,8 @@ class Markers_OF:  # Optical Flow
             cv2.imshow("Optical Flow", frame_curr_with_flow_vectors)
         # endregion
 
-        stride = 2
-        # region Visualize: Keypoints Velocity
-        if len(self.velocity_queue) >= stride:
-            frame_curr_with_velocity_keypoints = self.frame_queue[-1].copy()
-            velocity_list = [self.velocity_queue[-i - 1] for i in range(stride)]
-            velocity_list = np.mean(np.stack(velocity_list), axis=0)
-            for i, (keypoint, velocity) in enumerate(zip(curr_keypoints, velocity_list)):
-                if curr_keypoints_mask[i]:
-                    a, b = keypoint.flatten()
-                    c, d = velocity.flatten()
-                    a, b, c, d = int(a), int(b), int(c), int(d)
-                    cv2.arrowedLine(
-                        frame_curr_with_velocity_keypoints,
-                        (a, b),
-                        (a + c, b + d),
-                        (127, 253, 244),  # Kaguya Luna Color
-                        2,
-                        cv2.LINE_AA,
-                        tipLength=0.25,
-                    )
-            cv2.imshow("Velocity", frame_curr_with_velocity_keypoints)
-        # endregion
-
-        # region Visualize: Keypoints Acceleration:
-        if len(self.acceleration_queue) >= stride:
-            frame_curr_with_acceleration_keypoints = self.frame_queue[-1].copy()
-            acceleration_list = [self.acceleration_queue[-i - 1] for i in range(stride)]
-            acceleration_list = np.mean(np.stack(acceleration_list), axis=0)
-            for i, (keypoint, acceleration) in enumerate(zip(curr_keypoints, acceleration_list)):
-                if curr_keypoints_mask[i]:
-                    a, b = keypoint.flatten()
-                    c, d = acceleration.flatten() * 0.1  # Scale down the acceleration
-                    a, b, c, d = int(a), int(b), int(c), int(d)
-                    cv2.arrowedLine(
-                        frame_curr_with_acceleration_keypoints,
-                        (a, b),
-                        (a + c, b + d),
-                        (193, 242, 214),  # rurudo Color
-                        2,
-                        cv2.LINE_AA,
-                        tipLength=0.25,
-                    )
-            cv2.imshow("Acceleration", frame_curr_with_acceleration_keypoints)
-        # endregion
-
         # region WindowMovement
         cv2.moveWindow("Optical Flow", 200, 100)
-        cv2.moveWindow("Velocity", 600, 100)
-        cv2.moveWindow("Acceleration", 1000, 100)
         # endregion
 
         if self.is_timing:
@@ -244,13 +141,9 @@ class Markers_OF:  # Optical Flow
             self.frame_queue.clear()
             self.keypoints_queue.clear()
             self.keypoints_mask_queue.clear()
-            self.velocity_queue.clear()
-            self.acceleration_queue.clear()
             self.is_first_frame = True
             self.is_reset = False
         else:
-            self.calc()
-            self.clustering()
             if self.is_visualize:
                 self.visualize()
             # raise RuntimeError("Stop")
