689130bf1ce6fce919887cfb592a2e50cc590855
Zejun Yu <snda2008@live.cn>
2023-02-22 19:46:59 +0800

    update record_digit for gsmini

--- record_digit.py
+++ record_digit.py
@@ -1,60 +1,57 @@
 import time
 from global_params import *
-from connect_digit import connectDigit
+from connect_gsmini import connectGSmini
 import os
 import cv2
 
 
-def setVideoEncoder(fileName, ifVGA=False, scale=2):
+def setVideoEncoder(fileName):
     fps = videoFPS
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     videoOut = cv2.VideoWriter()
     tempName = "output/tmp.mp4"
-    if ifVGA:
-        videoOut.open(
-            tempName,
-            fourcc,
-            fps,
-            (480 * scale, 640 * scale),
-        )
-    else:
-        videoOut.open(
-            tempName,
-            fourcc,
-            fps,
-            (240 * scale, 320 * scale),
-        )
+    videoOut.open(
+        tempName,
+        fourcc,
+        fps,
+        (320, 240),
+    )
     return videoOut, tempName, fileName
 
 
 def main():
-    digit = connectDigit(intensity)
-    for _ in range(60):  # Preheat the digit
-        frm0 = digit.get_frame()
+    gsmini = connectGSmini()
+
+    for _ in range(5):
+        _ = gsmini.get_image((384, 288))
 
     timestr = time.strftime("%Y%m%d-%H%M%S")
     fileName = "output/recordDigit-{}.mp4".format(timestr)
-    videoOut, videotempName, videofileName = setVideoEncoder(fileName, scale=1)
+    videoOut, videotempName, videofileName = setVideoEncoder(fileName)
 
-    while True:
-        frm = digit.get_frame()
+    try:
+        while True:
+            frm = gsmini.get_image((384, 288))
 
-        cv2.imshow("recordDigit", frm)
-        cv2.moveWindow("recordDigit", 2020, 100)
+            cv2.imshow("recordDigit", frm)
+            cv2.moveWindow("recordDigit", 220, 100)
 
-        videoOut.write(frm)
+            videoOut.write(frm)
 
-        getKey = cv2.waitKey(1)
+            getKey = cv2.waitKey(1)
+            if getKey == 27 or getKey == ord("q") or getKey == ord("s"):  # ESC or q or s
+                break
+    except KeyboardInterrupt:
+        print("Keyboard Interrupt")
+    finally:
+        cv2.destroyAllWindows()
+        gsmini.stop_video()
+        videoOut.release()
         if getKey == ord("s"):
-            os.rename(videotempName, videofileName)
             print("Video saved to output/")
-            break
-        if getKey == 27 or getKey == ord("q"):  # ESC or q
-            break
-
-    digit.disconnect()
-    videoOut.release()
-    os.remove(videotempName)
+            os.rename(videotempName, videofileName)
+        else:
+            os.remove(videotempName)
 
 
 if __name__ == "__main__":
